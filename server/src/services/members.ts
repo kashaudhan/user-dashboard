27dff4f01255f2293c9dd03416dcba9dc75f655f
Dharmendra Kashaudhan <kashaudhan94@gmail.com>
2024-06-07 14:20:29 +0530

    restructure

--- server/src/services/members.ts
+++ server/src/services/members.ts
@@ -10,22 +10,27 @@ export const getMembers = async (req: Request, res: Response) => {
           m.id,
           m.name,
           m.user_name,
-          m.avatar,
           m.is_active,
           m.role,
           m.email,
           COALESCE(json_agg(t.team) FILTER (WHERE t.team IS NOT NULL), '[]') AS teams
         FROM member m
-        LEFT JOIN team t ON m.id = t.member_id
+        LEFT JOIN member_team t ON m.id = t.member_id
         WHERE m.is_deleted = FALSE
         GROUP BY m.id
         ORDER BY m.id
         LIMIT 10 OFFSET $1
       )
       SELECT json_agg(member_team) FROM member_team;
-    `)
-  } catch (error) {
+    `, [0]);
+
+    return res.status(200).json({
+      data: result.rows[0].json_agg
+    }).end();
     
+  } catch (error) {
+    console.error(error);
+    return res.status(500).end();
   }
 }
 
@@ -71,7 +76,43 @@ export const getMember = async (req: Request, res: Response) => {
 }
 
 export const addMember = async (req: Request, res: Response) => {
-  const { name, user_name, is_active, email, role,  } = req.body;
+  const { name, user_name, email, role, team } = req.body;
+
+  try {
+
+    await db.query('BEGIN');
+
+    const result = await db.query(`
+      INSERT INTO member
+        (name, user_name, email, role, is_active, is_deleted)
+        VALUES($1, $2, $3, $4, $5, $6)
+      RETURNING *;
+      `, [name, user_name, email, role, true, false]);
+
+    const memberId = result.rows[0].id;
+
+    const teamData = await db.query(`
+      INSERT INTO member_team
+        (member_id, team)
+        VALUES($1, $2)
+      RETURNING *;
+    `, [ memberId, team ]);
+
+    await db.query('COMMIT');
+
+    return res.status(201).json({
+      data: {
+        ...result.rows[0],
+        team: [teamData.rows[0].team]
+      }
+    })
+    
+  } catch (error) {
+    await db.query('ROLLBACK');
+    console.log("Error: ", error)
+    return res.status(500).end();
+  }
+
 }
 
 export const updateMember = async (req: Request, res: Response) => {
